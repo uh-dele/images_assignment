8b1bfc01158156872e0a79c9fc7cbf1ea87fa1c7
uh-dele <kikus832@newschool.edu>
2016-03-08 17:43:29 -0500

    Updated

--- sketch.js
+++ sketch.js
@@ -8,25 +8,44 @@
 
 //STEP 1: Declare global variables for your images and upload button
 
+var uploadButton
+	img
+	grumpyCatR
+ 	grumpyCatL;
+
+
 function preload(){
 	//STEP 2:
 	// Load your image with loadImage("path/to/image.jpg")
 	//and save it to your global variable:
 
-
 }
 
 function setup() {
-
 	//STEP 3
 	//add a file upload button with CreateFileInput(_____);
 
 	//STEP 4:
   //set up your canvas with createCanvas(__,__);
-
+ uploadButton = createFileInput(imageUploaded);
 }
 
+function imageUploaded(file) {
+	img = loadImage (file.data, drawMyImage);
+}
+
+//function drawMyImage (){
+	//image (img);
+
+//}
+
 function draw() {
+
+	if (img){
+		image(img);
+		background(img);
+	}
+
 
 	//STEP 5:
 	//draw your images with image(imagevariable);
@@ -43,3 +62,6 @@ function draw() {
 //which will be called when the user uploads an image:
 
 	//and inside it you will need to save the loaded image to a global variable
+
+
+//	The design thinking of the past has always pushed for more time for productive thinking, holidng de-stressed thinking as an essential part of the INNOVATION process. This article,
