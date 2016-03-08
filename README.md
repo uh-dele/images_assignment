8dc94a44e98c9a139d33d72c4e7e3d65c90303f7
Alison Abreu-Garcia <alison.iag@gmail.com>
2016-03-08 14:46:00 -0500

    initialize

---
+++ README.md
@@ -0,0 +1,13 @@
+Images in p5
+============
+
+In order to run your program you will need to download Anvil, which lets you run a server on your computer.
+
+http://anvilformac.com/
+
+Once you download it, if your computer doesn't want to open it, right click on the application and select "open."
+
+Make sure your anvil server is on when viewing the examples and working on your assignment. Once you add your project folder to Anvil, you'll be able to access your program at http://images.dev
+
+
+[View assignment instruction on the class website](http://creativecomputing-s16.github.io/2015/03/08/images.html)
---
+++ sketch.js
@@ -0,0 +1,45 @@
+//================================
+//	view your project using anvil at:
+//	http://images.dev
+//
+// or after you upload it to github at:
+//	http://yourusername.github.io/images/
+//================================
+
+//STEP 1: Declare global variables for your images and upload button
+
+function preload(){
+	//STEP 2:
+	// Load your image with loadImage("path/to/image.jpg")
+	//and save it to your global variable:
+
+
+}
+
+function setup() {
+
+	//STEP 3
+	//add a file upload button with CreateFileInput(_____);
+
+	//STEP 4:
+  //set up your canvas with createCanvas(__,__);
+
+}
+
+function draw() {
+
+	//STEP 5:
+	//draw your images with image(imagevariable);
+
+	//STEP 7:
+	//You're also going to need an if statement here to check
+	//if the image the user uploaded exists to then draw it
+	//(see image upload example version 2)
+
+}
+
+//STEP 6:
+//create a callback function (you can name it anything you'd like)
+//which will be called when the user uploads an image:
+
+	//and inside it you will need to save the loaded image to a global variable
